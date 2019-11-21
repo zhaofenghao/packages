c164b09951fbb28b1af556a3d2a37539226d3c23
zfh <zhaofenghao@corp-ci.com>
2019-11-21 11:45:45 +0800

    first commit

---
+++ First.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zfh
+ * Date: 2019-11-21
+ * Time: 11:44
+ */
+class First
+{
+    public function __construct()
+    {
+        // ..
+    }
+
+    // ... some functions
+}
